ba9e10e8f20ad845965b6156732d1ec6adb25eee
Nick Cho <nicholas.cho@hotmail.ca>
2024-08-22 14:08:10 -0400

    CSES Gray Code

---
+++ CSES/grayCode.py
@@ -0,0 +1,31 @@
+def main():
+    n = int(input())
+    def rec(n):
+        if n == 1:
+            return ["0", "1"]
+        
+        prevGrayCode = rec(n-1)
+        reversedPrevGrayCode = prevGrayCode[::-1]
+        
+        prevSize = len(prevGrayCode)
+        index = 0
+        print("Reversed: " , reversedPrevGrayCode)
+        while index < prevSize:
+            print(prevGrayCode)
+            appendedZero = "0" + prevGrayCode[index]
+            
+            prevGrayCode[index] = "1" + reversedPrevGrayCode[index]
+            
+            prevGrayCode.append(appendedZero)
+            
+            print(prevGrayCode)
+            
+            index += 1
+            
+        return prevGrayCode
+    arr = rec(n)
+    for num in arr:
+        print(num)
+    
+if __name__ == "__main__": 
+    main()  
