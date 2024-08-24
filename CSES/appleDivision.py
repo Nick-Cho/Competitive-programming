f2ed47d4375a1a3ff6386594901029f258a4ff57
Nick Cho <nicholas.cho@hotmail.ca>
2024-08-24 18:11:34 -0400

    CSES - apple division

--- CSES/appleDivision.py
+++ CSES/appleDivision.py
@@ -3,12 +3,17 @@ def main():
     apples = []
     apples = list(map(int, input().split(" ")))
     # print(apples)
-    res = rec(0, 0, 0, 0)
-    def rec(i, sum1, sum2, diff):
+
+    def rec(i, sum1, sum2):
         if i == n:
-            return diff
+            return abs(sum1-sum2)
         
+        take = rec(i+1, sum1+apples[i], sum2)
+        not_take = rec(i+1, sum1, sum2+apples[i])
+        return min(take, not_take)
     
+    res = rec(0, 0, 0)
+    print(res)
     
 if __name__ == "__main__":
     main()
